791e2532a369ba756e87307ef4cff98947e5a240
Klaus Wenger <kjwenger@yahoo.com>
2017-08-23 20:52:12 +0200

    initial load

--- create.sh
+++ create.sh
@@ -1,4 +1,6 @@
-  wget -c http://cdn.sencha.com/cmd/6.5.1/no-jre/SenchaCmd-6.5.1-linux-amd64.sh.zip -P /tmp
-  unzip /tmp/SenchaCmd-6.5.1-linux-amd64.sh.zip -d /tmp
-  sudo /tmp/SenchaCmd-6.5.1.240-linux-amd64.sh
-  /opt/Sencha/Cmd/6.5.1.240/sencha generate app -ext sandbox.testrail.extjs6 .
+#!/usr/bin/env bash
+
+wget -c http://cdn.sencha.com/cmd/6.5.1/no-jre/SenchaCmd-6.5.1-linux-amd64.sh.zip -P /tmp
+unzip /tmp/SenchaCmd-6.5.1-linux-amd64.sh.zip -d /tmp
+sudo /tmp/SenchaCmd-6.5.1.240-linux-amd64.sh
+/opt/Sencha/Cmd/6.5.1.240/sencha generate app -ext sandbox.testrail.extjs6 .
